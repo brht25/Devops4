72d39ee0a35f7bb177badb6c843f351b92ac5106
brht25 <berhatyesilyurt@gmail.com>
2026-05-21 11:47:15 +0300

    7

--- src/main/java/com/example/demo/controller/MovieController.java
+++ src/main/java/com/example/demo/controller/MovieController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +10,6 @@ public class MovieController {
 
     @GetMapping({"/", "/api/movies"})
     public List<String> getMovies() {
-        return List.of("The Godfather", "Inception", "Interstellar");
+        return List.of("The Godfather", "Inception","iiiiiiiiii", "Interstellar");
     }
 }
